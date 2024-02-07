31222295b061d1312cad4bf01fb0250aecb98aaf
sushilbhattarai45 <bhattaraisushil005@gmail.com>
2024-02-07 12:39:24 +0545

    Image Gone Wrong

--- hashyy/src/components/api/aiSummerise.jsx
+++ hashyy/src/components/api/aiSummerise.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 export default async function AiSummerise(text) {
+  let text1 = text.replace(/[^a-zA-Z0-9]/g, " ");
+
   const options = {
     method: "POST",
     url: import.meta.env.VITE_AI_SUMMERIZATION_API_URL,
@@ -9,17 +11,24 @@ export default async function AiSummerise(text) {
       "X-RapidAPI-Key": import.meta.env.VITE_AI_SUMMERIZATION_X_RapidAPI_Key,
       "X-RapidAPI-Host": import.meta.env.VITE_AI_SUMMERIZATION_X_RapidAPI_Host,
     },
+    // data: {
+
+    // query:
+    //   "Summerize this blog  in not less than 70 words but not more than 90 words. Word limit is strictly 100 words. Dont answer anything rather than the beautifull summary itself. No links too :" +
+    //   text1,
+    // },
     data: {
-      query:
-        "Hey can you do this task for me, please. Summerize this blog  in not less than 70 words but not more than 90 words. Word limit is strictly 100 words. Dont answer anything rather than the beautifull summary itself. No links too :" +
-        text,
+      lang: "en",
+      text:
+        "Summerize this blog  in not less than 70 words but not more than 90 words. Word limit is strictly 100 words. Dont answer anything rather than the beautifull summary itself. No links too :" +
+        text1,
     },
   };
 
   try {
     const response = await axios.request(options);
     console.log(response);
-    return response.data.response;
+    return response.data.summary;
   } catch (error) {
     console.error(error);
   }
